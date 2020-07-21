88de2aa15e81b949332b7ae90639ea155fb6e042
baker615512 <baker615512@gmail.com>
2020-07-21 17:47:01 -0500

    added login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,14 @@
 class UsersController < ApplicationController
 
+    get '/users/login' do
+        erb :'/users/login'
+    end
+
+    post '/users/login' do
+
+    end
+
+    get 'users/signup' do
+
+    end
 end
--- config.ru
+++ config.ru
@@ -6,4 +6,6 @@ end
 
 use Rack::MethodOverride
 
+use UsersController
+use GamesController
 run ApplicationController
