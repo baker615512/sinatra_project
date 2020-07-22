e6d5a26e68acb33657f30ef5e435b8084abed928
baker615512 <baker615512@gmail.com>
2020-07-22 15:04:04 -0500

    finished login and added helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,22 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
   get "/" do
     erb :welcome
   end
 
+  helpers do
+
+    def current_user
+      User.find_by(id: session[:user_id]
+    end
+
+    def logged_in?
+      !!current_user
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,20 @@ class UsersController < ApplicationController
     end
 
     post '/users/login' do
+        @user = User.find_by(email: params[:email])
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id 
+            redirect "/users/#{user.id}"
+        else
 
+        end
     end
 
     get 'users/signup' do
 
+    end
+
+    get '/users/:id' do 
+        erb :'users/:id'
     end
 end
