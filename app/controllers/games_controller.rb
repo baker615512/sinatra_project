c01bfb49dc33b3646c941cfe4b106364fb04f76e
baker615512 <baker615512@gmail.com>
2020-07-23 21:59:22 -0500

    added edit game route

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,9 +10,18 @@ class GamesController < ApplicationController
         end
         if params[:game_name] != ""
             @game = Game.create(game_name: params[:game_name], user_id: current_user.id)
-            redirect "/games/#{@game.id}
+            redirect "/games/#{@game.id}"
         else
             redirect '/games/new'
         end
+    end
+
+    get '/games/:id' do
+        @game = Game.find_by(id: params[:id])
+        erb :'/games/show'
+    end
+
+    get '/games/:id/edit' do
+        erb :'/games/edit'
     end
 end
