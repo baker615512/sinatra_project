797a43813fdab47c63b32572d150c9dcd2e5df39
baker615512 <baker615512@gmail.com>
2020-07-26 17:08:49 -0500

    breaking stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,21 @@
 class UsersController < ApplicationController
 
-    get '/users/login' do
+    get '/login' do
         erb :'/users/login'
     end
 
-    post '/users/login' do
+    post '/login' do
         @user = User.find_by(email: params[:email])
         if @user.authenticate(params[:password])
             session[:user_id] = @user.id 
             redirect "/users/#{@user.id}"
         else
-            redirect '/users/login'
+            redirect '/login'
         end
     end
 
-    get '/users/signup' do
-        erb :'/users/signup'
+    get '/signup' do
+        erb :'/users/new'
     end
 
     post '/users' do
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
             session[:user_id] = @user.id 
             redirect "/users/#{@user.id}"
         else
-            redirect '/users/signup'
+            redirect '/signup'
         end
     end
 
